0dd97adf09d4107a0f529fb6c647047fad1262dd
Francis Alex Darang <francisalexddarang@gmail.com>
2025-10-05 22:09:33 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-3xl font-bold underline'>
+        Welcome to Trabho On The Go
+      </div>
+       
+    </>
+  )
+}
+
+export default App
